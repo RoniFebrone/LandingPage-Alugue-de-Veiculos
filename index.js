0f52d632ee12836cf4cb5c0a5af76101defcd45b
RoniFebrone <ronifebrone13@gmail.com>
2023-09-09 21:14:33 -0300

    firt version page

---
+++ index.js
@@ -0,0 +1,40 @@
+var card = window.document.getElementById("card")
+var back = window.document.getElementById("back")
+var next = window.document.getElementById("next")
+
+passarCard = () => {
+    card.src = 'Imagens/card-hondaCB.svg';
+    back.setAttribute("onclick", "voltarCard()")
+    next.setAttribute("onclick", "voltarCard()")
+
+}
+
+voltarCard = () => {
+    card.src = 'Imagens/card-scooter.svg';
+    back.setAttribute("onclick", "passarCard()")
+    next.setAttribute("onclick", "passarCard()")
+}
+
+adicionarHref = () => {
+    var linkHonda = document.getElementById("linkHonda");
+    var linkVoltz = document.getElementById("linkVoltz");
+
+    // Obtendo o src atual da imagem em destaque
+    var srcCardDestaque = document.getElementById("card").src;
+    //============================================================================
+    if (srcCardDestaque.endsWith('Imagens/card-scooter.svg')) {
+        linkVoltz.href = "https://wa.me/5521975877461?text=Quero+saber+mais+sobre+a+Voltz+EV1%21%21";
+        window.open(linkVoltz.href, "_blank");
+
+    //=============================================================================   
+    } else if (srcCardDestaque.endsWith('Imagens/card-hondaCB.svg')) {
+        linkHonda.href = "https://wa.me/5521975877461?text=Quero+saber+mais+sobre+a+Honda+CB500X%21%21";
+        window.open(linkHonda.href, "_blank");
+    }
+}
+
+
+
+
+
+
