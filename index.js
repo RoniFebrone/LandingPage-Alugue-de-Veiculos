3356a678e5a51b01418da77e534a59ba178b8cfe
RoniFebrone <ronifebrone13@gmail.com>
2023-09-10 12:51:59 -0300

    New features

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 var card = window.document.getElementById("card")
 var back = window.document.getElementById("back")
 var next = window.document.getElementById("next")
+var carrosselAtivo = true;
+
+function alternarCarrossel() {
+    if (carrosselAtivo) {
+        passarCard();
+    } else {
+        voltarCard();
+    }
+    carrosselAtivo = !carrosselAtivo; // Inverte o estado do carrossel
+}
+
 
 passarCard = () => {
     card.src = 'Imagens/card-hondaCB.svg';
@@ -26,14 +37,22 @@ adicionarHref = () => {
         linkVoltz.href = "https://wa.me/5521975877461?text=Quero+saber+mais+sobre+a+Voltz+EV1%21%21";
         window.open(linkVoltz.href, "_blank");
 
-    //=============================================================================   
+        //=============================================================================   
     } else if (srcCardDestaque.endsWith('Imagens/card-hondaCB.svg')) {
         linkHonda.href = "https://wa.me/5521975877461?text=Quero+saber+mais+sobre+a+Honda+CB500X%21%21";
         window.open(linkHonda.href, "_blank");
     }
 }
 
+var carrossel = setInterval(alternarCarrossel, 3000)
 
+card.addEventListener("mouseover", () => {
+    clearInterval(carrossel);
+});
+
+card.addEventListener("mouseout",  () => {
+    carrossel = setInterval(alternarCarrossel, 3000);
+});
 
 
 
